c048c0cd521507f3a3d95a1c2e6d61dc3e916c1e
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-03-19 17:45:42 +0100

    add livereload task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
 const { watch, src, dest, parallel,series } = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 function scss() {
-  return src("./scss/*.scss")
+  return src("./scss/main.scss")
     .pipe(sass().on('error', sass.logError))
     .pipe(dest("./css"));
 }
 
+function liveReload() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+}
+
+function reload(done) {
+  browserSync.reload();
+  done();
+}
 
 // exports.default = sass
 
+watch("*", series(scss, reload));
+exports.default = parallel(scss, liveReload);
 
-watch('./scss/style.scss', scss)
-exports.default = scss
