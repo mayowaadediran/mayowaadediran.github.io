99fa58ea29e091d54c4aec50dcc39e9e4aa2a44e
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-03-28 09:52:47 +0100

    add dynamic page content functionalities

--- js/main.js
+++ js/main.js
@@ -1,15 +1,73 @@
-//Mobile Toggle
-const mobileToggle = document.getElementById('mobile-nav-toggle');
-const mobileNav = document.getElementById('mobile-nav__control');
+import pages  from "./pages.js";
 
-//target mobile nav toggle on click
-mobileToggle.addEventListener('click', event => {
+const mobileToggle = document.getElementById('mobile-nav-toggle');
+const navBar = document.getElementById('nav-links');
+const navLinks = document.querySelectorAll(".link")
+let main = document.getElementById("main");
+
+
+
+function toggleNav() {
   mobileToggle.classList.toggle('open');
-  mobileNav.classList.toggle('mobile-control')
+  navBar.classList.toggle('mobile-control');
+}
+
+function removeMobileNav() {
+  if (mobileToggle.classList.contains('open')) {
+    mobileToggle.classList.remove('open');
+    navBar.classList.remove('mobile-control');
+  }
+}
+
+function checkActiveLink () {
+  for (var i = 0; i < navLinks.length; i++) {
+    if (navLinks[i].classList.contains('active-link') ){
+      navLinks[i].classList.remove('active-link')
+    }
+  }
+}
+
+function addActiveLink () {
+  this.classList.add('active-link');
+}
+
+mobileToggle.addEventListener('click', toggleNav);
+window.addEventListener('resize', removeMobileNav);
+window.addEventListener('hashchange', function() {
+  let x = location.hash
+  let currentHash = x.substring(1);
+  let currentTag = document.getElementById(`${currentHash}`);
+  let page = pages[currentHash];
+  if (currentTag === null) {
+    main.innerHTML = "";
+    main.innerHTML = page;
+  }
+})
+window.addEventListener('hashchange', function() {
+  for (var i = 0; i < navLinks.length; i++) {
+    if (navLinks[i].classList.contains('active-link')) {
+      navLinks[i].classList.remove('active-link');
+    }
+  }
+})
+
+window.addEventListener('hashchange', function () {
+  for (var i = 0; i < navLinks.length; i++) {
+    if (navLinks[i].hash === location.hash) {
+      navLinks[i].classList.add('active-link');
+    }
+  }
+})
+
+
+navLinks.forEach(link => {
+  link.addEventListener('click', removeMobileNav);
+  link.addEventListener('click', checkActiveLink);
+  link.addEventListener('click', addActiveLink);
 });
 
-  //Toggle open classes 
-    //add open class to mobile nav toggle
-      //rotate the hamburger
-    //add open to mobile -links 
-      //the transform move to 0%;
+
+
+
+//on hash change
+//main.innerhtml will have the pages.splicethefirstcharacter of the hash
